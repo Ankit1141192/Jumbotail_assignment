f45f85e3d210df1303687576d583576c6013b9f6
Ankit Kumar <ankit2914978@gmail.com>
2026-02-05 13:51:12 +0530

    jumbotail

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,16 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-// DB
 const connectDB = require("./config/db");
 
-// Routes
 const productRoutes = require("./routes/productRoutes");
 const searchRoutes = require("./routes/searchRoutes");
 
-// Middleware
 const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
-
-// Connect Database
 connectDB();
 
-// Global Middlewares
 app.use(cors());
 app.use(express.json());
 
@@ -30,10 +24,10 @@ app.get("/health", (req, res) => {
   res.send("CatalogIQ API is up and running. You can proceed further.");
 });
 
-// Error Handler (ALWAYS at last)
+// Error Handler
 app.use(errorHandler);
 
-// Server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
